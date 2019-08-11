2bbf27f6a4ac00626966f5112506e526d7000d68
Manish Dhawal <manishdhawal88@gmail.com>
2019-08-11 18:18:20 +0530

    First commit

---
+++ scripts/forecast.js
@@ -0,0 +1,35 @@
+const key = 'xpvG8pYm61v8QAkehtlA0c7tTRvyPyOA';
+
+
+//get weather details
+const getWeather = async(id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+
+//get city
+const getCity = async(city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+    
+    return data[0];
+};
+
+// getCity('manchester').then((data) => {
+//         return getWeather(data.Key);
+//     }).then((data) => {
+//         console.log(data);
+        
+//     }).catch((err) => {
+//         console.log(err);
+        
+//     });
